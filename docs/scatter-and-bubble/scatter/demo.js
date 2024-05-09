97a8f6e9d39a67b422033ed4ab52358a716cf835
sam <sam@jennifersoft.com>
2024-05-09 14:53:17 +0900

    2초 주기로 차트 갱신

--- docs/scatter-and-bubble/scatter/demo.js
+++ docs/scatter-and-bubble/scatter/demo.js
@@ -47,98 +47,103 @@ async function getData() {
     return response.json();
 }
 
+updateChart();
+setInterval(updateChart, 2000);
 
-getData().then(data => {
-    const getData = (min, max) => {
-        const temp = [];
-        data.result.forEach(elm => {
-            if (elm.elapseTime >= min && elm.elapseTime < max) {
-                temp.push([elm.startTime, elm.elapseTime / 1000, { name: elm.application, domainId: elm.domainId, instanceId: elm.instanceId }]);
-            }
+
+function updateChart() {
+    getData().then(data => {
+        const getData = (min, max) => {
+            const temp = [];
+            data.result.forEach(elm => {
+                if (elm.elapseTime >= min && elm.elapseTime < max) {
+                    temp.push([elm.startTime, elm.elapseTime / 1000, { name: elm.application, domainId: elm.domainId, instanceId: elm.instanceId }]);
+                }
+            });
+            return temp;
+        };
+        series.forEach(s => {
+            s.data = getData(s.min, s.max);
         });
-        return temp;
-    };
-    series.forEach(s => {
-        s.data = getData(s.min, s.max);
-    });
-
-    Highcharts.chart('container', {
-        chart: {
-            type: 'scatter',
-            zoomType: 'xy'
-        },
-        title: {
-            text: '액티브 서비스',
-            align: 'left'
-        },
-        xAxis: {
-            type: 'datetime',
-            title: {
-                text: '시작시간'
+    
+        Highcharts.chart('container', {
+            chart: {
+                type: 'scatter',
+                zoomType: 'xy'
             },
-            labels: {
+            title: {
+                text: '액티브 서비스',
+                align: 'left'
+            },
+            xAxis: {
+                type: 'datetime',
+                title: {
+                    text: '시작시간'
+                },
+                labels: {
+                    formatter: function () {
+                        const date = new Date(this.value);
+                        const hours = date.getHours();
+                        const minutes = String(date.getMinutes()).padStart(2, '0');
+                        const seconds = String(date.getSeconds()).padStart(2, '0');
+                        return `${hours}:${minutes}:${seconds}`;
+                    }
+                },
+                startOnTick: true,
+                endOnTick: true,
+                showLastLabel: true
+            },
+            yAxis: {
+                title: {
+                    text: '응답시간'
+                },
+                labels: {
+                    format: '{value} s'
+                }
+            },
+            legend: {
+                enabled: true
+            },
+            plotOptions: {
+                series: {
+                    animation: false // 애니메이션 효과 없앰
+                },
+                scatter: {
+                    marker: {
+                        radius: 2.5,
+                        symbol: 'circle',
+                        states: {
+                            hover: {
+                                enabled: true,
+                                lineColor: 'rgb(100,100,100)'
+                            }
+                        }
+                    },
+                    states: {
+                        hover: {
+                            marker: {
+                                enabled: false
+                            }
+                        }
+                    },
+                    jitter: {
+                        x: 0.005
+                    }
+                }
+            },
+            tooltip: {
                 formatter: function () {
-                    const date = new Date(this.value);
+                    const date = new Date(this.x);
                     const hours = date.getHours();
                     const minutes = String(date.getMinutes()).padStart(2, '0');
                     const seconds = String(date.getSeconds()).padStart(2, '0');
-                    return `${hours}:${minutes}:${seconds}`;
+                    const formattedTime = `${hours}:${minutes}:${seconds}`;
+    
+                    return `<b>시작시간:</b> ${formattedTime}<br/><b>응답시간:</b> ${this.y}`;
                 }
             },
-            startOnTick: true,
-            endOnTick: true,
-            showLastLabel: true
-        },
-        yAxis: {
-            title: {
-                text: '응답시간'
-            },
-            labels: {
-                format: '{value} s'
-            }
-        },
-        legend: {
-            enabled: true
-        },
-        plotOptions: {
-            series: {
-                animation: false // 애니메이션 효과 없앰
-            },
-            scatter: {
-                marker: {
-                    radius: 2.5,
-                    symbol: 'circle',
-                    states: {
-                        hover: {
-                            enabled: true,
-                            lineColor: 'rgb(100,100,100)'
-                        }
-                    }
-                },
-                states: {
-                    hover: {
-                        marker: {
-                            enabled: false
-                        }
-                    }
-                },
-                jitter: {
-                    x: 0.005
-                }
-            }
-        },
-        tooltip: {
-            formatter: function() {
-                const date = new Date(this.x);
-                const hours = date.getHours();
-                const minutes = String(date.getMinutes()).padStart(2, '0');
-                const seconds = String(date.getSeconds()).padStart(2, '0');
-                const formattedTime = `${hours}:${minutes}:${seconds}`;
-
-                return `<b>시작시간:</b> ${formattedTime}<br/><b>응답시간:</b> ${this.y}`;
-            }
-        },
-        series
-    });
+            series
+        });
+    });    
 }
-);
+
